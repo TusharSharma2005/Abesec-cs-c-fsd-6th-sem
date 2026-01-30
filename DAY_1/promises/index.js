4a2d49a3d9564d749318484850b28fd1d776e346
tusha <tusharsharma20052010@gmail.com>
2026-01-30 09:12:49 +0530

    promises

---
+++ DAY_1/promises/index.js
@@ -0,0 +1,10 @@
+const myPromise=new Promise((resolve,reject)=>{
+    let marks=78;
+    if(marks>33){
+        resolve("pass");
+    }else{
+        reject("fail");
+    }
+})
+myPromise.then((msg)=>
+console.log(msg)).catch((error)=>console.log("some error occured",error));
